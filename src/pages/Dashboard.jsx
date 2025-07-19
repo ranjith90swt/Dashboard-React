79a74bd4eb06adaf3d487a24adcb974c6c9ef6a7
Anver Selva <anver.selva@craetiveeyetech.com>
2025-07-19 12:36:09 +0530

    updated

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Card from '../components/Card'
 import ProductList from './ProductList'
 import UserList from './UserList'
+import DashboardChart from '../components/DashboardChart'
 const Dashboard = () => {
   return (
     <>
