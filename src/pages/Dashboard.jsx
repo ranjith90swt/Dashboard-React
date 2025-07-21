6d7bb4c68d75564613a59bf094d51b087dcb1b1b
ranjith90swt <ranjith90swt@gmail.com>
2025-07-21 18:08:49 +0530

    css updateds

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,8 +9,8 @@ const Dashboard = () => {
     <>
       <h2 className='page-title'>Dashboard</h2>
 
-      <div className='d-flex flex-row gap-4 mb-4'>
-        <div className="w-25">
+      <div className='row g-3 mb-4'>
+        <div className="col-md-6 col-lg-3 col-sm-6">
           <StatCard 
                title="Total Users"
                 value={1240}
@@ -20,7 +20,7 @@ const Dashboard = () => {
           />
         </div>
 
-         <div className="w-25">
+         <div className="col-md-6 col-lg-3 col-sm-6">
             <StatCard 
                title="Total Revenue"
                 value={3240}
@@ -31,7 +31,7 @@ const Dashboard = () => {
             />
         </div>
 
-         <div className="w-25">
+         <div className="col-md-6 col-lg-3 col-sm-6">
             <StatCard 
                title="Total Transactions"
                 value={2245}
@@ -41,7 +41,7 @@ const Dashboard = () => {
             />
         </div>
 
-         <div className="w-25">
+         <div className="col-md-6 col-lg-3 col-sm-6">
             <StatCard 
                title="Total Amount"
                 value={3045}
@@ -53,17 +53,21 @@ const Dashboard = () => {
        
       </div>
 
-      <Card title="Chart">
+      <div className="row g-3">
+        <div className="col-lg-12">
+          <Card title="Chart">
+            <DashboardChart title='Chart' />
+          </Card>
+        </div>
+      </div>
 
-        <DashboardChart title='Chart' />
-      </Card>
 
       
 
 
 
-      <div className="d-flex flex-row gap-4 mt-4">
-        <div className="w-50">
+      <div className="row g-3 mt-2">
+        <div className="col-lg-6">
           
           <UserList 
            
@@ -79,7 +83,7 @@ const Dashboard = () => {
           
           
         </div>
-        <div className="w-50">
+        <div className="col-lg-6">
           <UserList 
            
            title='Recent Users'
@@ -93,6 +97,8 @@ const Dashboard = () => {
           />
         </div>
       </div>
+
+     
     </>
 
   )
