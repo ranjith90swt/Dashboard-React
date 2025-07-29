a291fb8aed8106f38e6bcbf906c17254d40b3c6d
ranjith90swt <ranjith90swt@gmail.com>
2025-07-29 14:27:53 +0530

    Chart changes

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+const useFetch = () => {
+  const [data, setData] = useState()
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null)
+
+  
+}
+
+export default useFetch
